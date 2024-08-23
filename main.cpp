c97a8e4d3e136e3ecdd86721bca94d24c9069c7f
IsacVera <isaacvera37@icloud.com>
2024-08-22 23:49:15 -0700

    feat: take in user input

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include "ChessBoard.h"
 
 int main() {
+    std::string userInput;
+
     ChessBoard chessBoard;
     chessBoard.resetBoard();
 
-    //chessBoard.displayBoard();
     chessBoard.displayBoard();
+    
+    std::cout << "Enter your postion: 'A8'" << std::endl;
+    std::cin >> userInput; 
+
+    std::cout << userInput;
 
     return 0;
 }
